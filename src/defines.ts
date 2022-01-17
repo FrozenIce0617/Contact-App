c672f101aa7a5b87c4fca0c80dc40fdf557d5c3c
SuccessTracker <prosdev087@gmail.com>
2022-01-17 19:41:40 +0000

    add types and init values for context provider

--- src/defines.ts
+++ src/defines.ts
@@ -1,2 +1,24 @@
+import { Store, IContactState, IContact } from "types";
+
 export const BASE_URL =
-"https://61c32f169cfb8f0017a3e9f4.mockapi.io/api/v1/contacts";
+  "https://61c32f169cfb8f0017a3e9f4.mockapi.io/api/v1/contacts";
+
+export const initialContext: IContactState = {
+  loading: false,
+  contacts: new Map<string, IContact>(),
+};
+
+export const initialStore: Store = {
+  state: initialContext,
+  dispatch: () => undefined,
+};
+
+export const getFormattedDate = (dateStr: string | undefined) => {
+  try {
+    if (!dateStr) return "";
+    const date = new Date(dateStr);
+    return date?.toDateString();
+  } catch (err: any) {
+    return "";
+  }
+};
--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,53 @@ export interface IContact {
   phone?: string;
   birthday?: string;
 }
+
+export enum ActionType {
+  SET_CONTACTS = "SET_CONTACTS",
+  ADD_CONTACT = "ADD",
+  UPDATE_CONTACT = "UPDATE",
+  REMOVE_CONTACT = "REMOVE",
+  SET_LOADING = "SET_LOADING",
+}
+
+export interface ISetContacts {
+  type: ActionType.SET_CONTACTS;
+  payload: IContact[];
+}
+
+export interface IAddContact {
+  type: ActionType.ADD_CONTACT;
+  payload: IContact;
+}
+
+export interface IUpdateContact {
+  type: ActionType.UPDATE_CONTACT;
+  payload: IContact;
+}
+
+export interface IRemoveContact {
+  type: ActionType.REMOVE_CONTACT;
+  payload: IContact;
+}
+
+export interface ISetLoading {
+  type: ActionType.SET_LOADING;
+  payload: boolean;
+}
+
+export type Actions =
+  | ISetContacts
+  | IAddContact
+  | IUpdateContact
+  | IRemoveContact
+  | ISetLoading;
+
+export interface IContactState {
+  loading: boolean;
+  contacts: Map<string, IContact>;
+}
+
+export interface Store {
+  state: IContactState;
+  dispatch: React.Dispatch<Actions>;
+}
