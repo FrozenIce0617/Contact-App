a7cbf8e7d9db8260205d52b939a2ccb7708c6f23
SuccessTracker <prosdev087@gmail.com>
2022-01-17 19:38:20 +0000

    define Contact type and add crud apis

---
+++ src/network/index.ts
@@ -0,0 +1,77 @@
+import { BASE_URL } from "defines";
+import { IContact } from "types";
+
+// parse the contact json data to IContact
+function parseContact(json: any): IContact {
+  return {
+    id: json.id,
+    name: json.name,
+    createdAt: json.createdAt,
+    avatar: json.avatar,
+    email: json.email,
+    phone: json.phone,
+    birthday: json.birthday,
+  };
+}
+
+/**
+ * Returns contact list.
+ *
+ * url: https://61c32f169cfb8f0017a3e9f4.mockapi.io/api/v1/contacts
+ */
+export async function getContacts(): Promise<IContact[]> {
+  const response = await fetch(`${BASE_URL}`);
+  const jsonResponse = await response.json();
+
+  const contacts: IContact[] = jsonResponse?.map((contact: any) =>
+    parseContact(contact)
+  );
+
+  return contacts || [];
+}
+
+/**
+ * Create contact.
+ *
+ * url: https://61c32f169cfb8f0017a3e9f4.mockapi.io/api/v1/contacts/:id
+ * method: POST
+ */
+export async function createContact(): Promise<IContact> {
+  const response = await fetch(`${BASE_URL}`, { method: "POST" });
+  const jsonResponse = await response.json();
+
+  const newContact: IContact = parseContact(jsonResponse);
+  return newContact || {};
+}
+
+/**
+ * Update contact.
+ *
+ * url: https://61c32f169cfb8f0017a3e9f4.mockapi.io/api/v1/contacts/:id
+ * method: PUT
+ */
+export async function updateContact(contact: IContact): Promise<IContact> {
+  const response = await fetch(`${BASE_URL}/${contact.id}`, {
+    method: "PUT",
+    body: JSON.stringify({ name: contact.name }),
+  });
+  const jsonResponse = await response.json();
+
+  const updatedContact: IContact = parseContact(jsonResponse);
+  return updatedContact || {};
+}
+
+/**
+ * Remove contact.
+ *
+ * url: https://61c32f169cfb8f0017a3e9f4.mockapi.io/api/v1/contacts/:id
+ * method: DELETE
+ */
+export async function removeContact(contact: IContact): Promise<IContact> {
+  const response = await fetch(`${BASE_URL}/${contact.id}`, {
+    method: "DELETE",
+  });
+  const jsonResponse = await response.json();
+  const removedContact: IContact = parseContact(jsonResponse);
+  return removedContact || {};
+}
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface IContact {
+  id: string;
+  createdAt?: string;
+  name?: string;
+  avatar?: string;
+  email?: string;
+  phone?: string;
+  birthday?: string;
+}
